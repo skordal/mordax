976b9a5f18784a21d012d30da7e2e20df60851f1
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2013-09-24 21:01:46 +0200

    Finish 16C750 UART driver

--- kernel/drivers/debug/uart-16c750.c
+++ kernel/drivers/debug/uart-16c750.c
@@ -4,6 +4,7 @@
 
 #include <debug.h>
 #include <mmu.h>
+#include <utils.h>
 
 #include <drivers/debug/uart-16c750.h>
 #include <drivers/debug/debug.h>
@@ -21,8 +22,53 @@ bool uart_16c750_initialize(struct dt_node * device_node)
 	}
 
 	memory = mmu_map_device((physical_ptr) memory_info[0], (size_t) memory_info[1]);
-	debug_printf("Debug UART memory area is at %x physical, %x virtual, length %d\n", memory_info[0], memory, memory_info[1]);
 
+	// Reset the controller:
+	memory[DEBUG_16C750_SYSC] |= 1 << DEBUG_16C750_SOFTRESET;
+	while((memory[DEBUG_16C750_SYSS] & (1 << DEBUG_16C750_RESETDONE)) == 0);
+
+	// Set up power management settings:
+	memory[DEBUG_16C750_SYSC] =
+		1 << DEBUG_16C750_AUTOIDLE |
+		1 << DEBUG_16C750_ENAWAKEUP |
+		2 << DEBUG_16C750_IDLEMODE; // Smart-idle mode
+
+	// Enable sleep mode:
+	const char * sleep_mode = dt_get_string_property(device_node, "mordax,uart-sleepmode");
+	if(sleep_mode == 0 || str_equals(sleep_mode, "enabled"))
+	{
+		memory[DEBUG_16C750_EFR] = 1 << DEBUG_16C750_ENHANCED_EN;
+		memory[DEBUG_16C750_IER] = 1 << DEBUG_16C750_SLEEP_MODE;
+		memory[DEBUG_16C750_EFR] = 0;
+	}
+
+	// Switch to configuration mode A:
+	memory[DEBUG_16C750_LCR] = 1 << DEBUG_16C750_DIV_EN;
+
+	// Enable the FIFOs:
+	memory[DEBUG_16C750_FCR] = 1 << DEBUG_16C750_FIFO_EN;
+
+	// Set the baudrate:
+	uint32_t baudrate = DEBUG_16C750_DEFAULT_BAUDRATE;
+	dt_get_array32_property(device_node, "mordax,uart-speed", &baudrate, 1);
+
+	uint16_t divisor = ((DEBUG_16C750_FCLK >> 4) / baudrate) & 0xffff;
+	memory[DEBUG_16C750_DLL] = divisor & 0xff;
+	memory[DEBUG_16C750_DLH] = divisor >> 8;
+
+	// Set the transmission parameters (including parity mode) - returns to operational mode:
+	const char * parity = dt_get_string_property(device_node, "mordax,uart-parity");
+	if(parity == 0 || str_equals(parity, "none"))
+		memory[DEBUG_16C750_LCR] = 3 << DEBUG_16C750_CHAR_LENGTH;
+	else if(str_equals(parity, "even"))
+		memory[DEBUG_16C750_LCR] = 3 << DEBUG_16C750_CHAR_LENGTH |
+			1 << DEBUG_16C750_PARITY_EN |
+			1 << DEBUG_16C750_PARITY_TYPE1;
+	else if(str_equals(parity, "odd"))
+		memory[DEBUG_16C750_LCR] = 3 << DEBUG_16C750_CHAR_LENGTH |
+			1 << DEBUG_16C750_PARITY_EN;
+
+	memory[DEBUG_16C750_MDR1] = 0; // Enable UART 16x mode
 	return true;
 }
 
--- kernel/drivers/debug/uart-16c750.h
+++ kernel/drivers/debug/uart-16c750.h
@@ -8,6 +8,47 @@
 #include <dt.h>
 #include <drivers/debug/debug.h>
 
+#define DEBUG_16C750_DEFAULT_BAUDRATE	115200
+#define DEBUG_16C750_FCLK		48000000
+
+// Register definitions:
+#define DEBUG_16C750_DLL	0x00
+#define DEBUG_16C750_DLH	0x04
+#define DEBUG_16C750_IER	0x04
+#define DEBUG_16C750_FCR	0x08
+#define DEBUG_16C750_EFR	0x08
+#define DEBUG_16C750_LCR	0x0c
+#define DEBUG_16C750_MDR1	0x20
+#define DEBUG_16C750_SYSC	0x54
+#define DEBUG_16C750_SYSS	0x58
+
+// IER bit names:
+#define DEBUG_16C750_SLEEP_MODE		4
+
+// EFR bit names:
+#define DEBUG_16C750_ENHANCED_EN	4
+
+// FCR bit names:
+#define DEBUG_16C750_FIFO_EN		0
+
+// LCR bit names:
+#define DEBUG_16C750_DIV_EN		7
+#define DEBUG_16C750_PARITY_TYPE1	4
+#define DEBUG_16C750_PARITY_EN		3
+#define DEBUG_16C750_CHAR_LENGTH	0
+
+// MDR1 bit names:
+#define DEBUG_16C750_MODE_SELECT	0
+
+// SYSC bit names:
+#define DEBUG_16C750_IDLEMODE		3
+#define DEBUG_16C750_ENAWAKEUP		2
+#define DEBUG_16C750_SOFTRESET		1
+#define DEBUG_16C750_AUTOIDLE		0
+
+// SYSS bit names:
+#define DEBUG_16C750_RESETDONE		0
+
 bool uart_16c750_initialize(struct dt_node * device_node);
 void uart_16c750_putc(char c);
 
