0ec0bff09abae126c73b20a1e1eb2766bf0ad7dc
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2013-11-11 22:24:30 +0100

    Add syscalls 0 (system) and 1 (thread exit)

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -2,18 +2,60 @@
 // (c) Kristian Klomsten Skordal 2013 <kristian.skordal@gmail.com>
 // Report bugs and issues on <http://github.com/skordal/mordax/issues>
 
-#include "debug.h"
 #include "context.h"
+#include "debug.h"
+#include "process.h"
+#include "scheduler.h"
 #include "syscall.h"
+#include "thread.h"
 
 // System call handler, called by target assembly code:
-void syscall_interrupt_handler(struct thread_context * context, uint8_t svc)
+void syscall_interrupt_handler(struct thread_context * context, uint8_t syscall)
 {
-	debug_printf("Supervisor call: %d, context at %p\n", svc, context);
-	debug_printf("\tArgument 1: %x\n", context_get_syscall_argument(context, 0));
-	debug_printf("\tArgument 2: %x\n", context_get_syscall_argument(context, 1));
-	debug_printf("\tArgument 3: %x\n", context_get_syscall_argument(context, 2));
-	debug_printf("\tReturning 0...\n");
-	context_set_syscall_retval(context, 0);
+	void * arguments[3] = {
+		context_get_syscall_argument(context, 0),
+		context_get_syscall_argument(context, 1),
+		context_get_syscall_argument(context, 2)};
+
+	debug_printf("System call from process %d thread %d: %d\n",
+		active_thread->parent->pid, active_thread->tid, syscall);
+
+	switch(syscall)
+	{
+		case SYSCALL_SYSTEM:
+			syscall_system(context, (int) arguments[0]);
+			break;
+		case SYSCALL_THREAD_EXIT:
+			syscall_thread_exit(context);
+			break;
+		default: // TODO: handle unrecognized system calls
+			debug_printf("Unknown system call %d\n", syscall);
+			break;
+	}
+}
+
+void syscall_system(struct thread_context * context, int function)
+{
+	switch(function)
+	{
+		case 0:
+		{
+			const char * string = context_get_syscall_argument(context, 1);
+			debug_printf("[SYSCALL0: DEBUG] PID %d, TID %d: %s\n", active_thread->parent->pid,
+				active_thread->tid, string);
+			break;
+		}
+		default:
+			debug_printf("Unknown function for the system syscall: %d\n", function);
+			break;
+	}
+}
+
+void syscall_thread_exit(struct thread_context * context)
+{
+	struct thread * removed_thread = scheduler_remove_thread(active_thread);
+
+	thread_free(removed_thread);
+	scheduler_reschedule();
 }
 
--- kernel/syscall.h
+++ kernel/syscall.h
@@ -12,6 +12,23 @@
  * @{
  */
 
+/** System syscall. Used for various administrative purposes. */
+#define SYSCALL_SYSTEM		0
+
+/** Thread exit syscall. This exits and frees the calling thread. */
+#define SYSCALL_THREAD_EXIT	1
+
+/**
+ * System syscall handler.
+ * @param context process context information.
+ */
+void syscall_system(struct thread_context * context, int function);
+
+/**
+ * Thread exit syscall handler.
+ * @param context process context information.
+ */
+void syscall_thread_exit(struct thread_context * context);
 
 /** @} */
 
