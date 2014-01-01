7f7b41809fb5b412ae2b3fe59059d4bf68b60d77
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2014-01-01 23:59:00 +0100

    Provide better error messages when handling memory aborts

--- kernel/abort.c
+++ kernel/abort.c
@@ -3,16 +3,30 @@
 // Report bugs and issues on <http://github.com/skordal/mordax/issues>
 
 #include "abort.h"
+#include "context.h"
 #include "debug.h"
+#include "kernel.h"
 
 bool abort_handler(struct thread_context * context)
 {
 	struct abort_details details;
 	abort_get_details(&details, context);
 
-	debug_printf("Invalid memory %s access at %p\n",
-		details.type == ABORT_READ ? "read" : "write",
-		details.address);
+	if(details.mode == ABORT_KERNEL)
+	{
+		debug_printf("\nINVALID MEMORY ACCESS IN KERNEL MODE\n");
+		debug_printf("Cannot %s address %x\n",
+			details.type == ABORT_READ ? "read from" : "write to",
+			details.address);
+		context_print(context);
+		kernel_panic("unrecoverable memory access error in kernel mode");
+	} else {
+		debug_printf("Invalid memory %s access at %p\n",
+			details.type == ABORT_READ ? "read" : "write",
+			details.address);
+		context_print(context);
+		kernel_panic("unrecoverable memory access error in user mode");
+	}
 
 	return false;
 }
