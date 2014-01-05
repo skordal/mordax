f4c6622b46657ea8f572290a561c51835af7c3b1
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2014-01-05 22:23:06 +0100

    Add memory mapping test and move test apps to their own directory

---
+++ applications/syscall_tests/map_test.c
@@ -0,0 +1,44 @@
+// The Mordax Microkernel OS Memory Mapping Test Programme
+// (c) Kristian Klomsten Skordal 2013 <kristian.skordal@gmail.com>
+// Report bugs and issues on <http://github.com/skordal/mordax/issues>
+
+#include <stdint.h>
+#include <mordax.h>
+
+// Size of the application image:
+extern void * image_size;
+
+int main(void)
+{
+	mordax_system(MORDAX_SYSTEM_DEBUG, "Mordax Developer Test Application");
+
+	// This test application only works on the BeagleBoard, as it uses the UART.
+
+	struct mordax_memory_attributes attributes = {
+		.type = MORDAX_TYPE_DEVICE,
+		.permissions = MORDAX_PERM_RW_RW
+	};
+	volatile char * uart = mordax_memory_map((void *) 0x20000000, (void *) 0x49020000,
+		4096, &attributes);
+	if((uint32_t) uart == 0x20000000)
+		mordax_system(MORDAX_SYSTEM_DEBUG, "Memory mapping succeeded");
+	else {
+		mordax_system(MORDAX_SYSTEM_DEBUG, "Memory mapping failed");
+		return 1;
+	}
+
+	// Test the mapped memory:
+	char * c = "test string\n\r";
+	for(int i = 0; i < 13; ++i)
+	{
+		while(!(uart[0x14] & (1 << 5)));
+		*uart = c[i];
+	}
+
+	mordax_system(MORDAX_SYSTEM_DEBUG, "Unmapping memory...");
+	mordax_memory_unmap((void *) uart, 4096);
+
+	mordax_system(MORDAX_SYSTEM_DEBUG, "Finished.");
+	return 0;
+}
+
